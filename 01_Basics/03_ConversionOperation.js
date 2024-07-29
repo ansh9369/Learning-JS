91f76f26ae317245937795f93ecf62856b11a599
ansh9369 <152509321+ansh9369@users.noreply.github.com>
2024-07-29 06:51:47 +0000

    Conversion of Data Type

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,30 @@
+// let score = "23fdx";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn = 1;
+// let isLoggedIn = "Ansh"
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(loggedIn);
+console.log(booleanIsLoggedIn);
+
+/*
+1 => true,  0 => false, 
+"" => false, "Ansh" => true
+*/
+
+//Changing String into number
+
+let someNumber = 5;
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// someNumber converted into string
