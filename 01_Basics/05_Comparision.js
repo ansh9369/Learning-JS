1e2d120c1a957afe4e544390faeae4534dbe650d
ansh9369 <152509321+ansh9369@users.noreply.github.com>
2024-07-29 07:27:02 +0000

    comparision

---
+++ 01_Basics/05_Comparision.js
@@ -0,0 +1,14 @@
+console.log(2 > 1);
+console.log("2" > 1);
+console.log("2" < 1);
+
+//CHecking  null is equal to 0 or not.
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// ===
+
+console.log("2" === 2);
+// they don't convert because they both have different data type. 
