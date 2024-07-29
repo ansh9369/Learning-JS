25729219b952e104f4f4577d0096067bf9c50c39
ansh9369 <152509321+ansh9369@users.noreply.github.com>
2024-07-29 07:14:30 +0000

    Operations

---
+++ 01_Basics/04_Operation.js
@@ -0,0 +1,32 @@
+//Assigning negative value 
+
+let value = 5;
+let negValue = -value;
+console.log(negValue);
+
+// Operations
+
+// console.log(2+7);
+// console.log(2-3);
+// console.log(2*2);
+// console.log(2/8);
+// console.log(13%5);
+
+let str1 = "Hello";
+let str2 = " Ansh";
+
+let addStr = str1 + str2;
+// console.log(addStr);
+
+console.log("1" + 2);
+
+// console.log("1" + 2 + 2);
+// they give us 112 output
+
+
+// console.log(1 + 2 + "2");
+//they give 32 because they see first number then, they add both number and again they add number with string. 
+
+
+// Arithimatic Operator
+console.log((3+7)* 7 % 6);
