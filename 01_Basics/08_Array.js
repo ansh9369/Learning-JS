45db9605e5454a011bd24cc7c45a9a9c7d2378fd
ansh9369 <152509321+ansh9369@users.noreply.github.com>
2024-07-30 12:28:08 +0000

    basic Arrays

---
+++ 01_Basics/08_Array.js
@@ -0,0 +1,29 @@
+//Array
+const myArr = [0,1,3,2,4,5]
+console.log(myArr);
+
+console.log(myArr.includes(4))  //Output is true.
+
+
+const Arr = ["AKM", "M24", "M249","M416"," "];
+console.log(Arr.indexOf('AKM'));      //Show index number.
+
+const mk1 = myArr.slice(1,3);
+console.log(mk1);
+
+
+//Array Method
+
+//push
+myArr.push(21);
+console.log(myArr);
+
+//pop
+myArr.pop(21);
+console.log(myArr);
+
+//unshift
+
+myArr.unshift(21);
+console.log(myArr);
+
