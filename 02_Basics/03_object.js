a160a81464eaee549dd8c008bc3e9f16514d1c79
ansh9369 <152509321+ansh9369@users.noreply.github.com>
2024-07-31 05:54:58 +0000

    destructor in object

--- 02_Basics/03_object.js
+++ 02_Basics/03_object.js
@@ -12,7 +12,7 @@ console.log(tinderUser);
 const product = {
     names:{
         id:{  
-        details: "cameraman"},
+        details: "camera"},
     },
     order_id: {
         sr: "123lele"
@@ -21,3 +21,34 @@ const product = {
 
 console.log(product.names.id.details);
 console.log(product.order_id.sr);
+
+
+//Object assign
+
+const object1 = {1: "a", 2: "b"};
+const object2 = {3: "c", 4: "d"};
+
+// const object3 = Object.assign({}, object1, object2);    // or instead of using this...we use spread operator----> 
+const object3 = {...object1, ...object2}
+
+console.log(object3);
+
+
+//Object destructure
+
+const tcs = {
+    courseName: "TCS ION",
+    prise: 2100,
+    courseInstructor: "NS"
+}
+
+const {courseInstructor: Mentor} = tcs;    //
+console.log(Mentor) 
+
+// JSON(JavaScript Object Notation)
+
+// {
+//     "name" : "ansh",
+//     "code" : 12234,
+//     "branch" = "IT"
+// }
