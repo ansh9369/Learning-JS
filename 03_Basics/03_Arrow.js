179f6bf6b4c19484a78c5930a0110405ae2ccf0c
ansh9369 <152509321+ansh9369@users.noreply.github.com>
2024-08-01 11:49:51 +0530

    this keyword

---
+++ 03_Basics/03_Arrow.js
@@ -0,0 +1,8 @@
+const user = {
+     userName: "Aditya",
+     price: 120;
+     welcomeMessage: function(){
+        console.log(`${this.userName}, welcome to our website.!!`)
+     }
+};
+user.welcomeMessage();
