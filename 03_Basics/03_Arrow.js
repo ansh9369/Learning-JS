b8c7d182ffbab6782fec5b652e968db20835f523
ansh9369 <152509321+ansh9369@users.noreply.github.com>
2024-08-02 08:33:44 +0000

    arrow

--- 03_Basics/03_Arrow.js
+++ 03_Basics/03_Arrow.js
@@ -1,6 +1,6 @@
 const user = {
      userName: "Aditya",
-     price: 120;
+     price: 120,
      welcomeMessage: function(){
         console.log(`${this.userName}, welcome to our website.!!`)
      }
