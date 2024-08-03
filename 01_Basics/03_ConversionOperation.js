06672c968b3fe3cb685195199120d9cb1a1f9f3a
ansh9369 <152509321+ansh9369@users.noreply.github.com>
2024-08-03 12:07:43 +0000

    update

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -1,18 +1,19 @@
-// let score = "23fdx";
+let score = "23fdx";
 
 // console.log(typeof score);
 // console.log(typeof(score));
 
-// let valueInNumber = Number(score);
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
 // let isLoggedIn = 1;
 // let isLoggedIn = "Ansh"
 let isLoggedIn = ""
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 // console.log(loggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 /*
 1 => true,  0 => false, 
@@ -24,7 +25,7 @@ console.log(booleanIsLoggedIn);
 let someNumber = 5;
 let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 // someNumber converted into string
