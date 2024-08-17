23584307396c20f839653a917c69b7b021b2d81e
Ansh <152509321+ansh9369@users.noreply.github.com>
2024-08-17 14:55:17 +0000

    Commit

--- 01_Basics/userInput.js
+++ 01_Basics/userInput.js
@@ -1,2 +1,6 @@
 let userInput = prompt("Please enter your name:");
 console.log("Hello, " + userInput + "!");
+
+let Input01 = prompt("Please put the input: ");
+console.log("Hello, " + Input01 + "!");
+
